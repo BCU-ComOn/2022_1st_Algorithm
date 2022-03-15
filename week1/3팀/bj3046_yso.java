75e295948e26ec8b5d71351644a82a2f5c2cce6c
SuOhYoon <100781721+SuOhYoon@users.noreply.github.com>
2022-03-15 18:10:05 +0900

    Add files via upload

---
+++ week1/3팀/bj3046_yso.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();	
+	System.out.println((S*2)-R1);
+}
+}	
