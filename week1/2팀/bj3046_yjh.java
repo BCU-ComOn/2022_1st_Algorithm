770c3bc44d4c728b0f2cb88b058d8824466265b2
Yuhahak <dbwogkr1214@naver.com>
2022-03-21 14:52:52 +0900

    1

---
+++ week1/2팀/bj3046_yjh.java
@@ -0,0 +1,21 @@
+package wwwe;
+
+import java.util.Scanner;
+
+public class bj4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner num = new Scanner(System.in);
+	
+		int r1 = num.nextInt();
+		int s = num.nextInt();
+		
+		System.out.println(s*2-r1);
+		
+		
+		
+	}
+
+}
