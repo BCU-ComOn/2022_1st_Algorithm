440c97383d7cdf3245825396abc8f4ee6709e6f5
Daniel-k-dev <88382464+Daniel-k-dev@users.noreply.github.com>
2022-03-22 18:00:55 +0900

    Update bj1712_kjy.java

--- week2/4팀/bj1712_kjy.java
+++ week2/4팀/bj1712_kjy.java
@@ -1,29 +1,34 @@
 package Week02;
 
 import java.util.Scanner;
-import java.math.BigInteger;
 
-public class bj1712_kjy {
-	
+public class Main {
+
 	public static void main(String[] args) {
+
+		Scanner in = new Scanner (System.in);	
+
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
 		
-		Scanner in = new Scanner (System.in);
-		int L = in.nextInt(); // L = 방학일수
-		int A = in.nextInt(); // A = 국어
-		int B = in.nextInt(); // B = 수학
-		int C = in.nextInt(); // C = 하루 최대 국어
-		int D = in.nextInt(); // D = 하루 최대 수학
-		int date = 0;
-		for (int day = L ; 0 < day ; day--) {
-			if (A <= 0 && B <= 0) {
-				date = day;
+		int status = 0;
+		int N = 0;
+		
+		int BEP = C - B;
+		int out = A + (B * N);
+		status = out;
+		while (true) {
+			N++;
+			if (BEP *N > out) {
+				System.out.println(N);
 				break;
 			}
-			else {
-				A -= C;
-				B -= D;
+			else if (status <= (out - BEP)) {
+				System.out.println("-1");
+				break;
 			}
 		}
-		System.out.println(date);
 	}
+		
 }
