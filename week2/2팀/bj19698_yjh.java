675005fbd588507820a10e726be35bc09f70d080
Yuhahak <dbwogkr1214@naver.com>
2022-03-28 23:19:03 +0900

    yjh

---
+++ week2/2팀/bj19698_yjh.java
@@ -0,0 +1,23 @@
+package wwwe;
+
+import java.util.Scanner;
+
+public class bj19698 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		int n4 = sc.nextInt();
+		
+		System.out.println((n2/n4)*(n3/n4));
+		
+				
+				
+
+	}
+
+}
