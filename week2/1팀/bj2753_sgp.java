ff498e56c0c1385f430d694aeac4b91c083b72e7
SeongGiPil <rlvf1234@naver.com>
2022-03-29 12:59:22 +0900

    week2
    week2

---
+++ week2/1팀/bj2753_sgp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in) ;
+		int y=sc.nextInt();
+		
+		if(y % 4 == 0) {
+		if(y%100!=0|| y%400==0) {
+			System.out.println("1");
+		    }
+        }else {
+			System.out.println("0");   
+		}
+	}
+}
