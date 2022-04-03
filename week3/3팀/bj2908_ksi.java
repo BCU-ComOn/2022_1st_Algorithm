b2ff9223500ebc185c0c56e1b31727dcd883e15f
KImSuim <suim7171.naver.com>
2022-04-03 14:11:48 +0900

    Update bj2908_ksi.java

---
+++ week3/3팀/bj2908_ksi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.print(A > B ? A : B);	
+	}
+
+}
