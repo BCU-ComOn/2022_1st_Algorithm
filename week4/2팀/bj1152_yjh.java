65e1494f690790b4058726099dfbd47f2404f3e0
Yuhahak <dbwogkr1214@naver.com>
2022-04-05 17:04:33 +0900

    yjh

---
+++ week4/2팀/bj1152_yjh.java
@@ -0,0 +1,26 @@
+package wwwe;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class bj1152 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String st = sc.nextLine();
+		sc.close();
+		
+		StringTokenizer s = new StringTokenizer(st," ");
+		
+		System.out.println(s.countTokens());
+		
+		
+		
+		
+		
+	}
+
+}
